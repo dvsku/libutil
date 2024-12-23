87970a714a2e07ce7972dcd952c645f321f34366
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-12-23 16:24:16 +0100

    Update assert

--- source/libutil/libutil/env/assert.hpp
+++ source/libutil/libutil/env/assert.hpp
@@ -1,6 +1,23 @@
 #pragma once
 
-#include <stdexcept>
+#include "libutil/env/env.hpp"
 
-#define LIBUTIL_ASSERT(expr, message)   \
-    if (!(expr)) throw std::runtime_error(message);
+#if defined(DV_OS_WINDOWS) && defined(DV_DEBUG)
+    #include <windows.h>
+    #include <stdexcept>
+
+    #define LIBUTIL_ASSERT(statement, message)                                  \
+        if (!(statement)) {                                                     \
+            MessageBox(NULL, message, "Assert fail", MB_OK | MB_ICONERROR);     \
+            throw std::runtime_error(message);                                  \
+        }
+#elif defined(DV_DEBUG)
+    #include <stdexcept>
+
+    #define LIBUTIL_ASSERT(statement, message)                                  \
+        if (!(statement)) {                                                     \
+            throw std::runtime_error(message);                                  \
+        }
+#else
+    #define LIBUTIL_ASSERT(statement, message) do {} while(0)
+#endif
